308408b7bf2d6c97ded2337a3b653f345c4dba1c
alexanderdeoz <sad.chinchin@yavirac.edu.ec>
2021-01-05 20:08:09 -0500

    trabajado en clase

---
+++ Adt/Adt/Nodo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Adt
+{
+    public class Nodo
+    {
+        //creacion de un arbol
+        public string Nombre { get; set; }
+        //public List<Nodo> Hijos { get; set; }
+
+        public Nodo Izquierdo { get; set; }
+        public Nodo Derecho { get; set; }
+    }
+}
---
+++ Adt/AdtApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using Adt;
+using System;
+
+namespace AdtApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Arbol Binario");
+
+            var raiz = new Nodo();
+            raiz.Nombre = "1";
+
+            var nodoDos = new Nodo();
+            nodoDos.Nombre = "1.1";
+
+            var nodoTres = new Nodo();
+            nodoTres.Nombre = "1.2";
+
+            raiz.Izquierdo = nodoDos;
+            raiz.Derecho = nodoTres;
+
+            //metodo imprimir recursivo
+
+            AdministradorNodo an =  new AdministradorNodo();
+            an.Imprimir(raiz);
+
+
+
+        }
+    }
+}
